dc0a900384f648bc233ea7bbc35da5a6454be454
muhammadedowardaya <muhammadedowarday4@gmail.com>
2022-06-29 16:25:40 +0700

    memperbaiki desain

--- js/script.js
+++ js/script.js
@@ -1,151 +1,210 @@
-class Counter {
-    #counter = 0;
-
-    increment() {
-        this.#counter++;
+class CounterNumber extends HTMLElement {
+    connectedCallback() {
+        this.number = 0;
+        this.displayNumber = "Nol";
+        this.render();
     }
 
-    decrement() {
-        this.#counter--;
+    set number(value) {
+        this._number = value;
+        this.render();
     }
 
-    get() {
-        return this.#counter;
+    set displayNumber(value) {
+        this._displayNumber = value;
+        this.render();
+    }
+
+    set onClickPlus(value) {
+        this._onClickPlus = value;
+        this.render();
+    }
+
+    set onClickMinus(value) {
+        this._onClickMinus = value;
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+            <section class="content-main">
+                <header>
+                    <div class="number">
+                        <span>${this._displayNumber}</span>
+                    </div>
+                </header>
+                <figure>
+                    <div class="main-counter">
+                        <h3>${this._number}</h3>
+                    </div>
+                    <div class="main-action">
+                        <button class="main-button__minus">-</button>
+                        <button class="main-button__plus">+</button>
+                    </div>
+                </figure>
+            </section>
+        `;
+        this.querySelector(".main-button__plus").addEventListener(
+            "click",
+            this._onClickPlus
+        );
+        this.querySelector(".main-button__minus").addEventListener(
+            "click",
+            this._onClickMinus
+        );
     }
 }
 
-class Person {
+customElements.define("counter-number", CounterNumber);
+
+const Person = {
     gender(gender) {
         this.gender = gender;
-    }
-}
+    },
+};
 
-const user = new Person();
+const Counter = {
+    number: 0,
 
+    increment() {
+        this.number++;
+    },
 
-const counter = new Counter();
+    decrement() {
+        this.number--;
+    },
 
-const nilai = document.querySelector('header .counter h3');
-const pesan = document.querySelector('main .pesan span');
+    get() {
+        return this.number;
+    },
+};
 
-const kurang = document.querySelectorAll('header .button button')[0];
-const tambah = document.querySelectorAll('header .button button')[1];
+const User = {
+    gender(gender) {
+        this.gender = gender;
+    },
+};
+
+const counterNumber = document.querySelector("counter-number");
 
 function isiPesan() {
     let sapa = "";
-    if(user.gender === 'pria'){
+    if (User.gender === "pria") {
         sapa = "Tamvans";
-    }else if(user.gender === 'wanita'){
+    } else if (user.gender === "wanita") {
         sapa = "Cangtip";
-    }else{
+    } else {
         sapa = "Hey!";
     }
-    switch (counter.get()) {
+    switch (Counter.get()) {
         case -1:
-            counter.increment();
-            nilai.textContent = 0;
-            pesan.textContent = `Nanaonan maneh ${sapa}, moal bisa kana mines! wkwkwkwkwk`;
+            Counter.increment();
+            counterNumber.number = 0;
+            counterNumber.displayNumber = `Nanaonan maneh ${sapa}, moal bisa kana mines! wkwkwkwkwk`;
             break;
         case 0:
-            pesan.textContent = "Nol";
+            counterNumber.displayNumber = "Nol";
             break;
         case 1:
-            pesan.textContent = "Hiji";
+            counterNumber.displayNumber = "Hiji";
             break;
         case 2:
-            pesan.textContent = "Dua";
+            counterNumber.displayNumber = "Dua";
             break;
         case 3:
-            pesan.textContent = "Tilu";
+            counterNumber.displayNumber = "Tilu";
             break;
         case 4:
-            pesan.textContent = "Opat";
+            counterNumber.displayNumber = "Opat";
             break;
         case 5:
-            pesan.textContent = "Lima";
+            counterNumber.displayNumber = "Lima";
             break;
         case 6:
-            pesan.textContent = "Genep";
+            counterNumber.displayNumber = "Genep";
             break;
         case 7:
-            pesan.textContent = "Tujuh";
+            counterNumber.displayNumber = "Tujuh";
             break;
         case 8:
-            pesan.textContent = "Dalapan";
+            counterNumber.displayNumber = "Dalapan";
             break;
         case 9:
-            pesan.textContent = "Sambilan";
+            counterNumber.displayNumber = "Sambilan";
             break;
         case 10:
-            pesan.textContent = "Sapuluh";
+            counterNumber.displayNumber = "Sapuluh";
             break;
         case 11:
-            pesan.textContent = "Geus ah cape, nepi ka sapuluh weh wkwk";
-            counter.decrement();
-            nilai.textContent = 10;
+            counterNumber.displayNumber =
+                "Geus ah cape, nepi ka sapuluh weh wkwk";
+            Counter.decrement();
+            counterNumber.number = 10;
             break;
         default:
-            pesan.textContent = "";
+            counterNumber.displayNumber = "";
             break;
     }
 }
 
-tambah.addEventListener('click', function () {
-    if (counter.get() <= 10) {
-        counter.increment();
-        nilai.textContent = counter.get();
+counterNumber.onClickPlus = () => {
+    if (Counter.get() <= 10) {
+        Counter.increment();
+        counterNumber.number = Counter.get();
     }
+    isiPesan();
+};
 
-    isiPesan();
-})
-kurang.addEventListener('click', function () {
-    if (counter.get() >= 0) {
-        counter.decrement();
+counterNumber.onClickMinus = () => {
+    if (Counter.get() >= 0) {
+        Counter.decrement();
     }
     isiPesan();
-    nilai.textContent = counter.get();
-})
+    counterNumber.number = Counter.get();
+};
 
 Swal.fire({
-    title: 'Pilih Gender',
+    title: "Pilih Gender",
     text: "Pria atau Wanita?",
-    icon: 'question',
+    icon: "question",
     showCancelButton: false,
     showDenyButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Pria',
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Pria",
     denyButtonText: `wanita`,
 }).then((result) => {
     if (result.isConfirmed) {
-        user.gender('pria');
+        User.gender("pria");
         Swal.fire({
-            title: 'Hai Tamvans :v',
-            text: 'Tapi lebih tamvan ice bear sih',
-            imageUrl: 'https://muhammadedowardaya.github.io/counter/img/tamvans.png',
+            title: "Hai Tamvans :v",
+            text: "Tapi lebih tamvan ice bear sih",
+            imageUrl:
+                "https://muhammadedowardaya.github.io/counter/img/tamvans.png",
             showCancelButton: false,
-            confirmButtonColor: '#3085d6',
-            confirmButtonText: 'Sa Ae Bhujank',
-        })
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Sa Ae Bhujank",
+        });
     } else if (result.isDenied) {
-        user.gender('wanita');
+        User.gender("wanita");
         Swal.fire({
-            title: 'Hai Cangtip :)',
-            imageUrl: 'https://muhammadedowardaya.github.io/counter/img/cangtip.png',
+            title: "Hai Cangtip :)",
+            imageUrl:
+                "https://muhammadedowardaya.github.io/counter/img/cangtip.png",
             showCancelButton: false,
-            confirmButtonColor: '#3085d6',
-            confirmButtonText: 'Makasih, Ini udah bawaan lahir',
-        })
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Makasih, Ini udah bawaan lahir",
+        });
     } else {
-        user.gender();
+        User.gender();
         Swal.fire({
-            title: 'Tch! Ice bear tydak suka!',
+            title: "Tch! Ice bear tydak suka!",
             text: "kenapa kau tidak memilihnya? Ice bear harap kau bukan g** awokwokwk",
-            imageUrl: 'https://muhammadedowardaya.github.io/counter/img/tolak.png',
+            imageUrl:
+                "https://muhammadedowardaya.github.io/counter/img/tolak.png",
             showCancelButton: false,
-            confirmButtonColor: '#3085d6',
-            confirmButtonText: 'Urraaaa',
-        })
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Urraaaa",
+        });
     }
-
-})
+});
